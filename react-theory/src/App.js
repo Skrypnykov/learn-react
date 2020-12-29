0acfa7eefcb61964e6045a368771d441ac71a496
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-12-29 23:23:47 +0200

    add new component

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    return (
+      <div style={divStyle}>
+        <div className="container-content">
+          <h1 className="header-title">Hello, World!</h1>
+        </div>
+        <Car />
+        <Car />
+      </div>
+    );
+  }
 }
 
 export default App;
