1f1c76215773cd07b1327b177aaf8d8842aa1ecb
Oleh Skrypnykov <oskripnikov@gmail.com>
2021-01-04 20:35:08 +0200

    add air-auqlity-react

---
+++ air-quality-react/src/components/AirQuality/AirQuality.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useRef, useState} from 'react';
+import mapboxgl from 'mapbox-gl';
+import './AirQuality.scss';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2tyeXBueWtvdiIsImEiOiJja2ZsYTdkbzEwZGdqMnFwc2dyMHBnZTl0In0.XQ6qUunUpxoUThIMrGLbPQ';
+
+const AirQuality = () => {
+	const mapContainer = useRef(null);
+	const [locationInfo] = useState({
+		lng: 31,
+		lat: 48.55,
+		zoom: 5,
+		scrollZoom: false
+	});
+	
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: 'mapbox://styles/mapbox/outdoors-v11',
+			center: [locationInfo.lng, locationInfo.lat],
+			zoom: locationInfo.zoom,
+			scrollZoom: locationInfo.scrollZoom
+		});
+
+		map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+		
+	}, [])
+	
+	return (
+		<section className={'air-q container'}>
+			<h2 className="air-q-title">Индекс качества воздуха в режиме реального времени</h2>
+			
+			<div className="air-q-map" ref={mapContainer}/>
+			
+			<p className="air-q-text">Для того чтобы узнать уровень загрязнения атмосферного воздуха (качество воздуха) в городе Киев, необходимо выбрать соответствующую станцию мониторинга на карте выше.</p>
+		</section>
+	);
+};
+
+export default AirQuality;
